1f0ff008b28061abbeadab695b5bd4a284c0b47b
Akram Elgyar <akramelgyar@gmail.com>
2023-03-21 18:03:17 -0700

    fix2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,10 @@ int print_last_digit(int n)
 	int LastDigit;
 
 	if (n < 0)
-		lastDigit = -1 * (n % 10);
+		LastDigit = -1 * (n % 10);
 	else
 		LastDigit = n % 10;
+
 	_putchar(LastDigit + '0');
 	return (lastDigit);
 }
