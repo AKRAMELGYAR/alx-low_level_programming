905f595b40961282e6baa4ab2fec7e51805fd224
Akram Elgyar <akramelgyar@gmail.com>
2023-03-21 17:58:34 -0700

    test last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "mani.h"
+
+/**
+ * print_last_digit - print last digit of a number.
+ *
+ * @n: takes number input
+ *
+ * Return: Lastdigit
+*/
+
+int print_last_digit(int n)
+{
+	int LastDigit;
+
+	if (n < 0)
+		lastDigit = -1 * (n % 10);
+	else
+		LastDigit = n % 10;
+	_putchar(LastDigit + '0');
+	return (lastDigit);
+}
