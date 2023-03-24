be0e9deeb1554b06a07a816fca13592e216fbdc8
Akram Elgyar <akramelgyar@gmail.com>
2023-03-23 17:38:11 -0700

    1-isdigit.c         2-mul.c         3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *            only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+		printf(i);
+	_putchar('\n');
+}
