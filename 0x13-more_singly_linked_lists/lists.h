43bd453fd8feee7337d40e644df888505e69ff60
AKRAMELGYAR <akramelgyar@gmail.com>
2023-05-02 03:21:25 -0700

    fix1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct listint_s - singly linked lists
- * @x: int
+ * @n: int
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -13,7 +13,7 @@
 
 typedef struct listint_s
 {
-	int x;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
